f44951af844e38e90453f09ca13ca6c11ec7e1fd
andreiscurtu-AS <andrei@digitalinzare.ro>
2025-07-24 21:03:36 +0300

    Create hivebrite.js

---
+++ hivebrite.js
@@ -0,0 +1,6 @@
+<script>
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.jsdelivr.net/gh/andreiscurtu-AS/hb-fonts@main/font-replacement.js';
+document.body.appendChild(script);
+</script>
